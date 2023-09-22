0eeae7773ce49e5b4cda53b8972d973e1fac557b
peterchavasse <pchavasse@temenos.com>
2023-09-22 11:17:07 +0100

    Support elements in shadow dom
    By passing a shadow DOM root as an option ("context"), source and destinations which are not visible to the main document can be found

--- src/lib/arguments/rectangle_from_param.js
+++ src/lib/arguments/rectangle_from_param.js
@@ -2,11 +2,12 @@ const Rectangle = require('./../geometry/rectangle');
 const Errors = require('./../util/errors');
 const isCoordinate = require('./../geometry/is_coordinate');
 
-function rectangleFromParam(param){
+function rectangleFromParam(param, context){
   if (isCoordinate(param)) {
     return new Rectangle(param.x, param.y, 0, 0);
   }
-  const element = document.querySelector(param);
+  const rootElement = context || document;
+  const element = rootElement.querySelector(param);
   if (!element) {
     Errors.couldNotFindSelector(param);
   }
